4bed44337bc2480fa0d85ce7bbbb972ae3b5c9fe
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-06 15:55:36 +0000

    Empty implementation of Contact interface

--- src/impl/ContactImpl.java
+++ src/impl/ContactImpl.java
@@ -1,7 +1,32 @@
 package impl;
 
+import spec.Contact;
+
 /**
- * Created by lmignot on 06/02/2016.
+ * Implementation of Contact interface
+ *
+ * @see spec.Contact;
+ * @author lmignot
  */
-public class ContactImpl {
+public class ContactImpl implements Contact {
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getNotes() {
+        return null;
+    }
+
+    @Override
+    public void addNotes(String note) {
+
+    }
 }
