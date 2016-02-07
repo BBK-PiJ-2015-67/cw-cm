d9cc290bc03f8e6228d58e83f702727a2e179e31
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-07 15:06:47 +0000

    Decided to implement adding contacts to CM as other methods depend on contacts being present Added a basic test for adding contacts

--- src/test/ContactManagerTest.java
+++ src/test/ContactManagerTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import spec.ContactManager;
 
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -30,7 +29,37 @@ public class ContactManagerTest {
     @Test
     public void constructAContactManager () {
         ContactManager cm = new ContacManagerImpl();
+        assertThat(cm).isNotNull();
+    }
+
+    @Test
+    public void constructAUniqueContactManager () {
+        ContactManager cm = new ContacManagerImpl();
+        cMgr = new ContacManagerImpl();
 
         assertThat(cm).isNotNull();
+
+        assertThat(cMgr).isNotNull();
+        assertThat(cMgr).isNotEqualTo(cm);
+    }
+
+    @Test
+    public void testAddContactsToCM () {
+        cMgr = new ContacManagerImpl();
+
+        int contact1 = cMgr.addNewContact("Xenia Garand", "This one's xenophobic");
+        int contact2 = cMgr.addNewContact("Sherlene Westrich", "From the west");
+        int contact3 = cMgr.addNewContact("Emmaline Cupit", "Cupid's daughter");
+        int contact4 = cMgr.addNewContact("Kendra Kinghorn", "Said to hold the secret to the legendary horn");
+        int contact5 = cMgr.addNewContact("Aaron Kamen", "Camen get it!");
+        int contact6 = cMgr.addNewContact("Ellis Pollak", "Unfortunate naming...");
+        int contact7 = cMgr.addNewContact("Carrol Sin", "Christmas is his favourite time");
+        int contact8 = cMgr.addNewContact("Norman Wiedemann", "XXXXL");
+        int contact9 = cMgr.addNewContact("Efren Apodaca", "There's a pharmacist in his future...");
+        int contact10 = cMgr.addNewContact("Floyd Drager", "In France we say this man is popular with..");
+        
+        assertThat(cMgr.getContacts("")).isNotEmpty();
+        assertThat(cMgr.getContacts("")).isNotNull();
+        assertThat(cMgr.getContacts("").size()).isEqualTo(10);
     }
 }
