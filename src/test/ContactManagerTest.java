d0d247fce5fb289d9c86fdd4fe00406883849971
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-07 14:47:09 +0000

    basic contact manager construction

--- src/test/ContactManagerTest.java
+++ src/test/ContactManagerTest.java
@@ -3,6 +3,10 @@ package test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import spec.ContactManager;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * ContactManager tests
@@ -11,18 +15,22 @@ import org.junit.Test;
  */
 public class ContactManagerTest {
 
+    private ContactManager cMgr;
+
     @Before
     public void setUp() {
-
+        cMgr = new ContactManagerImpl();
     }
 
     @After
     public void tearDown() {
-
+        cMgr = null;
     }
 
     @Test
     public void constructAContactManager () {
-        
+        ContactManager cm = new ContacManagerImpl();
+
+        assertThat(cm).isNotNull();
     }
 }
