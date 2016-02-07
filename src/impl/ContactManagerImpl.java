df138fba8e9e39156c75994d44f567c919d15cb9
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-07 15:14:44 +0000

    Add default interface methods so we can compile

--- src/impl/ContactManagerImpl.java
+++ src/impl/ContactManagerImpl.java
@@ -1,6 +1,10 @@
 package impl;
 
-import spec.ContactManager;
+import spec.*;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A Contact Manager implementation
@@ -11,4 +15,68 @@ import spec.ContactManager;
  */
 public class ContactManagerImpl implements ContactManager {
     
+    @Override
+    public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+        return 0;
+    }
+
+    @Override
+    public PastMeeting getPastMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public FutureMeeting getFutureMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public Meeting getMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getFutureMeetingList(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getMeetingListOn(Calendar date) {
+        return null;
+    }
+
+    @Override
+    public List<PastMeeting> getPastMeetingListFor(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) {
+
+    }
+
+    @Override
+    public void addMeetingNotes(int id, String text) {
+
+    }
+
+    @Override
+    public int addNewContact(String name, String notes) {
+        return 0;
+    }
+
+    @Override
+    public Set<Contact> getContacts(String name) {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts(int... ids) {
+        return null;
+    }
+
+    @Override
+    public void flush() {
+
+    }
 }
