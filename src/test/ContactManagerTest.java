7ea2edee4c75bf50ae42f5d83b4b43199593752b
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-07 14:35:00 +0000

    class to test a ContactManager

---
+++ src/test/ContactManagerTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * ContactManager tests
+ *
+ * @author lmignot
+ */
+public class ContactManagerTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void constructAContactManager () {
+        
+    }
+}
