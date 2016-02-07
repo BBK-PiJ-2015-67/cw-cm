fb3b2d4b8d38f020f7238d65e0a2a42e2176b2cf
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-07 16:03:33 +0000

    Test retrieving contacts by name

--- src/test/ContactManagerTest.java
+++ src/test/ContactManagerTest.java
@@ -4,7 +4,10 @@ import impl.ContactManagerImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import spec.Contact;
 import spec.ContactManager;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,13 +31,13 @@ public class ContactManagerTest {
     }
 
     @Test
-    public void constructAContactManager () {
+    public void testConstructAContactManager () {
         cMgr = new ContactManagerImpl();
         assertThat(cMgr).isNotNull();
     }
 
     @Test
-    public void constructAUniqueContactManager () {
+    public void testConstructAUniqueContactManager () {
         ContactManager cm = new ContactManagerImpl();
 
         assertThat(cm).isNotNull();
@@ -66,6 +69,31 @@ public class ContactManagerTest {
         assertThat(cMgr.getContacts("")).isEmpty();
         assertThat(cMgr.getContacts("")).isNotNull();
         assertThat(cMgr.getContacts("").size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetSpecificContactsFromCM () {
+        int a = cMgr.addNewContact("Aaron Kamen", "Camen get it!");
+        int b = cMgr.addNewContact("Aaron Kamen", "Camen get it!");
+        int c = cMgr.addNewContact("Xenia Garand", "This one's xenophobic");
+        int d = cMgr.addNewContact("Sherlene Westrich", "From the west");
+        int e = cMgr.addNewContact("Emmaline Cupit", "Cupid's daughter");
+        int f = cMgr.addNewContact("Kendra Kinghorn", "Said to hold the secret to the legendary horn");
+        int g = cMgr.addNewContact("Aaron Kamen", "Camen get it!");
+        int h = cMgr.addNewContact("Ellis Pollak", "Unfortunate naming...");
+        int i = cMgr.addNewContact("Carrol Sin", "Christmas is his favourite time");
+        int j = cMgr.addNewContact("Norman Wiedemann", "XXXXL");
+        int k = cMgr.addNewContact("Efren Apodaca", "There's a pharmacist in his future...");
+        int l = cMgr.addNewContact("Floyd Drager", "In France we say this man is popular with..");
+
+        Set<Contact> testContacts = cMgr.getContacts("Aaron Kamen");
+
+        assertThat(testContacts).isNotEmpty();
+        assertThat(testContacts).isNotNull();
+        assertThat(testContacts.size()).isEqualTo(3);
+
+        assertThat(a).isNotEqualTo(b).isNotEqualTo(g);
+        assertThat(b).isNotEqualTo(g);
     }
 
     @Test(expected = NullPointerException.class)
