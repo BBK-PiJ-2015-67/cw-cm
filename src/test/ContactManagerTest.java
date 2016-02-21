8ac22ae931fea28422f71306a859773d235650f4
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-21 10:54:05 +0000

    tests for getFutureMeetingList(Contact)

--- src/test/ContactManagerTest.java
+++ src/test/ContactManagerTest.java
@@ -1,15 +1,13 @@
 package test;
 
+import impl.ContactImpl;
 import impl.ContactManagerImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import spec.*;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -235,10 +233,75 @@ public class ContactManagerTest {
     }
     
     /* =================== MEETING LISTS =================== */
+
+    @Test
+    public void testGetEmptyFutureMeetingListIsEmpty() {
+        Set<Contact> contactSet = cMgrHasContacts.getContacts(1);
+        Contact testContact = null;
+
+        // seems odd that we're using a set - there's no "get()" method!!!
+        // so we have to loop!
+        for (Contact c: contactSet) {
+            testContact = new ContactImpl(c.getId(), c.getName(), c.getNotes());
+        }
+
+        List<Meeting> futureMeetingList = cMgrHasContacts.getFutureMeetingList(testContact);
+
+        assertNotNull(futureMeetingList);
+        assertEquals(futureMeetingList.size(), 0);
+        assertTrue(futureMeetingList.isEmpty());
+    }
     
     @Test
-    public void getFutureMeetingList() {
-        
+    public void testGetFutureMeetingListIsNotEmptyAndSorted() {
+        Set<Contact> contactSet = cMgrHasContacts.getContacts(1);
+        Set<Contact> meetingSet = cMgrHasContacts.getContacts(1,3,4,6,7,8);
+        Contact testContact = null;
+
+        // seems odd that we're using a set - there's no "get()" method!!!
+        // so we have to loop... to get ONE contact!
+        for (Contact c: contactSet) {
+            testContact = new ContactImpl(c.getId(), c.getName(), c.getNotes());
+        }
+
+        // PAST
+        Calendar date1 = new GregorianCalendar();
+        date1.set(Calendar.DATE, pastDate.get(Calendar.DATE));
+        // FUTURE
+        Calendar futureDate2 = new GregorianCalendar();
+        futureDate2.set(Calendar.DATE, futureDate.get(Calendar.DATE) + 5);
+        Calendar futureDate3 = new GregorianCalendar();
+        futureDate3.set(Calendar.DATE, futureDate2.get(Calendar.DATE) + 2);
+        // PAST
+        Calendar date4 = new GregorianCalendar();
+        date4.set(Calendar.DATE, futureDate3.get(Calendar.DATE) - 21);
+
+        cMgrHasContacts.addFutureMeeting(meetingSet, futureDate); // ID: 1
+        cMgrHasContacts.addNewPastMeeting(meetingSet, date1, "Notes about meeting 1"); // ID: 2
+        cMgrHasContacts.addFutureMeeting(meetingSet, futureDate2); // ID: 3
+        cMgrHasContacts.addFutureMeeting(meetingSet, futureDate3); // ID: 4
+        cMgrHasContacts.addNewPastMeeting(meetingSet, date4, "Notes about meeting 4"); // ID: 5
+        List<Meeting> futureMeetingList = cMgrHasContacts.getFutureMeetingList(testContact);
+
+        assertNotNull(futureMeetingList);
+        assertEquals(futureMeetingList.size(), 3);
+        assertFalse(futureMeetingList.isEmpty());
+
+        Meeting mtg1 = futureMeetingList.get(0);
+        Meeting mtg4 = futureMeetingList.get(1);
+        Meeting mtg3 = futureMeetingList.get(2);
+
+        assertEquals(mtg1.getId(), 1);
+        assertEquals(mtg3.getId(), 3);
+        assertEquals(mtg4.getId(), 4);
+
+        assertEquals(mtg1.getContacts(), meetingSet);
+        assertEquals(mtg3.getContacts(), meetingSet);
+        assertEquals(mtg4.getContacts(), meetingSet);
+
+        assertEquals(mtg1.getDate(), futureDate);
+        assertEquals(mtg3.getDate(), futureDate2);
+        assertEquals(mtg4.getDate(), futureDate3);
     }
 
     /* =================== CONTACTS =================== */
@@ -256,14 +319,14 @@ public class ContactManagerTest {
         int contact9 = cMgr.addNewContact("Efren Apodaca", "There's a pharmacist in his future...");
         int contact10 = cMgr.addNewContact("Errol Flynn", "The ultimate swashbuckler");
 
-        assertNotEquals(cMgr.getContacts(""), new HashSet<Contact>());
+        assertFalse(cMgr.getContacts("").isEmpty());
         assertNotNull(cMgr.getContacts(""));
         assertEquals(cMgr.getContacts("").size(),10);
     }
 
     @Test
     public void testThatNewCMHasEmptyContacts () {
-        assertEquals(cMgr.getContacts(""),new HashSet<Contact>());
+        assertTrue(cMgr.getContacts("").isEmpty());
         assertNotNull(cMgr.getContacts(""));
         assertEquals(cMgr.getContacts("").size(),0);
     }
@@ -285,7 +348,7 @@ public class ContactManagerTest {
 
         Set<Contact> testContacts = cMgr.getContacts("Aaron Kamen");
 
-        assertNotEquals(testContacts, new HashSet<Contact>());
+        assertFalse(testContacts.isEmpty());
         assertNotNull(testContacts);
         assertEquals(testContacts.size(),3);
 
@@ -303,7 +366,7 @@ public class ContactManagerTest {
 
         Set<Contact> testContacts = cMgr.getContacts("Sherlene Westrich");
 
-        assertNotEquals(testContacts, new HashSet<Contact>());
+        assertFalse(testContacts.isEmpty());
         assertNotNull(testContacts);
         assertEquals(testContacts.size(),2);
     }
@@ -312,7 +375,16 @@ public class ContactManagerTest {
     public void testGetContactsStringWhenCMHasNoContacts () {
         Set<Contact> testContacts = cMgr.getContacts("Floyd Drager");
 
-        assertEquals(testContacts, new HashSet<Contact>());
+        assertTrue(testContacts.isEmpty());
+        assertEquals(testContacts.size(),0);
+    }
+
+    @Test
+    public void testGetContactsStringWithNonExistentContact () {
+        Set<Contact> testContacts = cMgrHasContacts.getContacts("John Doe");
+
+        assertTrue(testContacts.isEmpty());
+        assertEquals(testContacts.size(),0);
     }
 
     @Test
