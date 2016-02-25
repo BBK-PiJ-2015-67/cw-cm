2cbdfd1d4dbec8cdd025b38cf1eb0c2cb8a4dced
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-02-25 14:19:19 +0000

    refactor future meeting test - using constants

--- src/test/FutureMeetingTest.java
+++ src/test/FutureMeetingTest.java
@@ -24,80 +24,101 @@ import static org.junit.Assert.assertNotNull;
  */
 public class FutureMeetingTest {
 
-    private Meeting mtg;
-    private int id = 72;
+    // some data to work with
+    private static final int MEETING_ID = 458;
+    private static final int MEETING_ID_NEG = -98;
+    private static final int MEETING_ID_ZERO = 0;
+    private static final int CONTACT_1_ID = 1;
+    private static final int CONTACT_2_ID = 2;
+    private static final int CONTACT_3_ID = 3;
+    private static final int CONTACT_4_ID = 4;
+    private static final String CONTACT_1_NAME = "Han Solo";
+    private static final String CONTACT_2_NAME = "Luke Skywalker";
+    private static final String CONTACT_3_NAME = "Princess Leia";
+    private static final String CONTACT_4_NAME = "Darth Vader";
+
+    private static final int EXPECTED_CONTACT_SET_SIZE = 4;
+
+    // for error checking
+    private static final Calendar NULL_CAL = null;
+    private static final Set<Contact> NULL_CONTACTS = null;
+    private static final Set<Contact> EMPTY_CONTACTS = new HashSet<>();
+
+    private static final int FUTURE_YEAR = 2020;
+    private static final int FUTURE_MONTH = 5;
+    private static final int FUTURE_DAY = 16;
+
     private Set<Contact> meetingContacts;
-    private Calendar date;
+    private Calendar futureDate;
+    private Meeting futureMeeting;
 
     @Before
     public void setUp () {
         meetingContacts = new HashSet<>();
-        date = new GregorianCalendar(2020, 2, 29, 6, 59, 0);
+        futureDate = new GregorianCalendar(FUTURE_YEAR, FUTURE_MONTH, FUTURE_DAY);
 
-        meetingContacts.add(new ContactImpl(23, "Jim Harrison"));
-        meetingContacts.add(new ContactImpl(33, "Van Wrinkle"));
-        meetingContacts.add(new ContactImpl(99, "Susan Doubtfire"));
-        meetingContacts.add(new ContactImpl(2, "Harry Smith"));
-        
-        mtg = new FutureMeetingImpl(id, date, meetingContacts);
+        meetingContacts.add(new ContactImpl(CONTACT_1_ID, CONTACT_1_NAME));
+        meetingContacts.add(new ContactImpl(CONTACT_2_ID, CONTACT_2_NAME));
+        meetingContacts.add(new ContactImpl(CONTACT_3_ID, CONTACT_3_NAME));
+        meetingContacts.add(new ContactImpl(CONTACT_4_ID, CONTACT_4_NAME));
     }
     
     @After
     public void tearDown () {
         meetingContacts = null;
-        mtg = null;
-        date = null;
+        futureMeeting = null;
+        futureDate = null;
     }
 
     @Test
     public void createAFutureMeeting() {
-        Meeting mMtg = new FutureMeetingImpl(48, new GregorianCalendar(2020, 2, 29, 6, 59, 0), meetingContacts);
+        Meeting mMtg = new FutureMeetingImpl(MEETING_ID, futureDate, meetingContacts);
 
         assertNotNull(mMtg);
-        assertEquals(mMtg.getId(),48);
-        assertEquals(mMtg.getDate(),new GregorianCalendar(2020, 2, 29, 6, 59, 0));
-        assertEquals(mMtg.getContacts().size(),4);
-        assertEquals(mMtg.getContacts(),meetingContacts);
+        assertEquals(mMtg.getId(), MEETING_ID);
+        assertEquals(mMtg.getDate(), futureDate);
+        assertEquals(mMtg.getContacts().size(), EXPECTED_CONTACT_SET_SIZE);
+        assertEquals(mMtg.getContacts(), meetingContacts);
     }
 
     @Test
     public void createAFutureMeetingOfTypeFutureMeeting() {
-        FutureMeeting fMtg = new FutureMeetingImpl(48, new GregorianCalendar(2020, 2, 29, 6, 59, 0), meetingContacts);
+        FutureMeeting fMtg = new FutureMeetingImpl(MEETING_ID, futureDate, meetingContacts);
 
         assertNotNull(fMtg);
-        assertEquals(fMtg.getId(),48);
-        assertEquals(fMtg.getDate(),new GregorianCalendar(2020, 2, 29, 6, 59, 0));
-        assertEquals(fMtg.getContacts().size(),4);
-        assertEquals(fMtg.getContacts(),meetingContacts);
+        assertEquals(fMtg.getId(), MEETING_ID);
+        assertEquals(fMtg.getDate(), futureDate);
+        assertEquals(fMtg.getContacts().size(), EXPECTED_CONTACT_SET_SIZE);
+        assertEquals(fMtg.getContacts(), meetingContacts);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void negativeIdShouldThrow () {
-        mtg = new FutureMeetingImpl(-2, date, meetingContacts);
+        futureMeeting = new FutureMeetingImpl(MEETING_ID_NEG, futureDate, meetingContacts);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void zeroIdShouldThrow () {
-        mtg = new FutureMeetingImpl(0, date, meetingContacts);
+        futureMeeting = new FutureMeetingImpl(MEETING_ID_ZERO, futureDate, meetingContacts);
     }
 
     @Test(expected = NullPointerException.class)
     public void nullDateShouldThrow () {
-        mtg = new FutureMeetingImpl(id, null, meetingContacts);
+        futureMeeting = new FutureMeetingImpl(MEETING_ID, NULL_CAL, meetingContacts);
     }
 
     @Test(expected = NullPointerException.class)
     public void nullContactsShouldThrow () {
-        mtg = new FutureMeetingImpl(id, date, null);
+        futureMeeting = new FutureMeetingImpl(MEETING_ID, futureDate, NULL_CONTACTS);
     }
 
     @Test(expected = NullPointerException.class)
     public void nullContactsAndDateShouldThrow () {
-        mtg = new FutureMeetingImpl(id, null, null);
+        futureMeeting = new FutureMeetingImpl(MEETING_ID, NULL_CAL, NULL_CONTACTS);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyContactsShouldThrow () {
-        mtg = new FutureMeetingImpl(id, date, new HashSet<>());
+        futureMeeting = new FutureMeetingImpl(MEETING_ID, futureDate, EMPTY_CONTACTS);
     }
 }
