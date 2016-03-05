98757dccc5560af276d6eda5c5292aacb9e4caf7
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2016-03-05 21:16:23 +0000

    added method to TestCommon to add a standard set of Meetings to a CM

--- src/test/TestCommon.java
+++ src/test/TestCommon.java
@@ -9,6 +9,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,32 +30,37 @@ final class TestCommon {
     static final int CONTACT_2_ID = 2;
     static final int CONTACT_3_ID = 3;
     static final int CONTACT_4_ID = 4;
+    static final int CONTACT_5_ID = 5;
+    static final int CONTACT_6_ID = 6;
     static final String CONTACT_1_NAME = "Wade Wilson";
     static final String CONTACT_2_NAME = "Peter Quill";
     static final String CONTACT_3_NAME = "Jessica Jones";
     static final String CONTACT_4_NAME = "Bruce Wayne";
+    static final String CONTACT_5_NAME = "Peter Parker";
+    static final String CONTACT_6_NAME = "Superman";
     static final String CONTACT_1_NOTES = "Deadpool";
     static final String CONTACT_2_NOTES = "Starlord";
     static final String CONTACT_3_NOTES = "No nickname";
     static final String CONTACT_4_NOTES = "Batman";
+    static final String CONTACT_5_NOTES = "Spiderman";
+    static final String CONTACT_6_NOTES = "Clark Kent";
 
     static final String CONTACT_3_NAME_WHITESPACE = "Jessica  Jones";
     static final String CONTACT_3_NAME_PREFIX = " Jessica Jones";
     static final String CONTACT_3_NAME_SUFFIX = "Jessica Jones ";
 
-    static final int EXPECTED_CONTACT_SET_SIZE = 4;
+    static final int EXPECTED_NUM_CONTACTS = 6;
+    static final int EXPECTED_NUM_CONTACTS_4 = 4;
     static final String NON_EXISTENT_CONTACT_NAME = "Matt Murdock";
 
     static final int MEETING_ID = 458;
     static final int FIRST_MEETING_ID = 1;
     static final int SECOND_MEETING_ID = 2;
-    static final int THIRD_MEETING_ID = 3;
-    static final int FOURTH_MEETING_ID = 4;
 
-    static final String MEETING_NOTES_1 = "Notes about meeting 1";
-    static final String MEETING_NOTES_2 = "Notes about meeting 2";
-    static final String MEETING_NOTES_3 = "Notes about meeting 3";
-    static final String MEETING_NOTES_4 = "Notes about meeting 4";
+    static final String MEETING_NOTES = "Notes";
+    static final String MEETING_NOTES_2 = "More Notes";
+    static final String MEETING_NOTES_3 = "More than more Notes";
+    static final String MEETING_NOTES_4 = "And some more notes. This can go on";
     static final String NOTES_DELIMITER = "\n";
 
     static final int FUTURE_YEAR = 2020;
@@ -80,12 +86,22 @@ final class TestCommon {
     static final int FIVE_MILLISECONDS = 5;
     static final int MINUS_FIVE_MILLISECONDS = -5;
 
-    static final int ILLEGAL_ID_1 = 6;
+    static final int ILLEGAL_ID_1 = 11;
     static final int ILLEGAL_ID_2 = 90;
     static final int ILLEGAL_ID_3 = 43;
     static final int ILLEGAL_ID_4 = 987;
     static final int ID_NEG = -2;
     static final int ID_ZERO = 0;
+    static final int ID_ONE = 1;
+    static final int ID_TWO = 2;
+    static final int ID_THREE = 3;
+    static final int ID_FOUR = 4;
+    static final int ID_FIVE = 5;
+    static final int ID_SIX = 6;
+    static final int ID_SEVEN = 7;
+    static final int ID_EIGHT = 8;
+    static final int ID_NINE = 9;
+    static final int ID_ELEVEN = 11;
 
     static final int EMPTY_SIZE = 0;
     static final String EMPTY_STRING = "";
@@ -132,11 +148,47 @@ final class TestCommon {
      *
      * @param cm The CM instance to which contacts will be added
      */
-    static void addFourContactsToCm(ContactManager cm) {
+    static void addTestContacts(ContactManager cm) {
         cm.addNewContact(CONTACT_1_NAME, CONTACT_1_NOTES);
         cm.addNewContact(CONTACT_2_NAME, CONTACT_2_NOTES);
         cm.addNewContact(CONTACT_3_NAME, CONTACT_3_NOTES);
         cm.addNewContact(CONTACT_4_NAME, CONTACT_4_NOTES);
+        cm.addNewContact(CONTACT_5_NAME, CONTACT_5_NOTES);
+        cm.addNewContact(CONTACT_6_NAME, CONTACT_6_NOTES);
     }
 
+    /**
+     * Adds meetings to a Contact Manager instance for testing
+     * As some tests require a CM instance to have a mixed set of meetings
+     * &ndash; this method helps to avoid code duplication in tests
+     *
+     * @param cm The CM instance to which meetings will be added
+     */
+    static void addTestMeetings(ContactManager cm, Set<Contact> contactsA, Set<Contact> contactsB) {
+        Calendar futureDate = new GregorianCalendar(FUTURE_YEAR, FUTURE_MONTH, FUTURE_DAY);
+        Calendar futureDate1 = new GregorianCalendar(FUTURE_YEAR, FUTURE_MONTH, FUTURE_DAY, HOUR_9, MINUTE_15);
+        Calendar futureDate2 = new GregorianCalendar(FUTURE_YEAR, FUTURE_MONTH, FUTURE_DAY, HOUR_11, MINUTE_20);
+        Calendar futureDate3 = new GregorianCalendar(FUTURE_YEAR, FUTURE_MONTH, FUTURE_DAY, HOUR_14, MINUTE_35);
+
+        Calendar pastDate = new GregorianCalendar(PAST_YEAR, PAST_MONTH, PAST_DAY);
+        Calendar pastDate1 = new GregorianCalendar(PAST_YEAR, PAST_MONTH, PAST_DAY, HOUR_9, MINUTE_15);
+        Calendar pastDate2 = new GregorianCalendar(PAST_YEAR, PAST_MONTH, PAST_DAY, HOUR_11, MINUTE_20);
+        Calendar pastDate3 = new GregorianCalendar(PAST_YEAR, PAST_MONTH, PAST_DAY, HOUR_14, MINUTE_35);
+
+        // mixed set of meetings for standard tests
+        cm.addFutureMeeting(contactsA, futureDate);
+        cm.addNewPastMeeting(contactsA, pastDate, MEETING_NOTES);
+        cm.addNewPastMeeting(contactsA, pastDate1, MEETING_NOTES);
+        cm.addFutureMeeting(contactsA, futureDate1);
+        cm.addFutureMeeting(contactsA, futureDate2);
+        cm.addNewPastMeeting(contactsA, pastDate2, MEETING_NOTES);
+        cm.addFutureMeeting(contactsA, futureDate3);
+        cm.addNewPastMeeting(contactsA, pastDate3, MEETING_NOTES);
+
+        // for duplication tests
+        cm.addFutureMeeting(contactsB, futureDate);
+        cm.addFutureMeeting(contactsB, futureDate);
+        cm.addNewPastMeeting(contactsB, pastDate, MEETING_NOTES);
+        cm.addNewPastMeeting(contactsB, pastDate, MEETING_NOTES);
+    }
 }
